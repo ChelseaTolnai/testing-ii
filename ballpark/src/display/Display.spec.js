38ef3d7e6301b1d521d44aa3995cd1dc3247f10a
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-06 20:55:24 -0500

    Added tests to Display component.

--- ballpark/src/display/Display.spec.js
+++ ballpark/src/display/Display.spec.js
@@ -4,11 +4,20 @@ import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect'; 
 
 import Display from './Display';
+import App from '../App'
 
 describe('<Display />', () => {
 
   it('renders without crashing', () => {
     render(<Display />);
   });
+
+  it('confirms table headers', () => {
+    const { getByText } = render(<Display />);
+    const balls = getByText(/balls/i);
+    const strikes = getByText(/strikes/i);
+    expect(balls).toBeInTheDocument();
+    expect(strikes).toBeInTheDocument();
+  })
 
 })
